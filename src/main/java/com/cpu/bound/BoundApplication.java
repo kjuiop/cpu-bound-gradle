d0777784f1247fc177917a2bd031da4713c19436
kjuiop <kjuiop@grovesoft.net>
2021-04-11 11:02:24 +0900

    Initial commit

---
+++ src/main/java/com/cpu/bound/BoundApplication.java
@@ -0,0 +1,13 @@
+package com.cpu.bound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpu/bound/BoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpu.bound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
